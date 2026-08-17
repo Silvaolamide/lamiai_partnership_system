cdac781ae57a33c1203e9c8526849bf6de4abf05
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-17 12:39:27 +0100

    Add business approval fields to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,13 +20,19 @@ class User extends Authenticatable implements MustVerifyEmail
 
     protected $fillable = [
         'name', 'email', 'password', 'business_name', 'business_website',
-        'business_industry', 'business_phone',
+        'business_industry', 'business_phone', 'business_super_admin_approved_at',
+        'business_rejected_at',
     ];
 
     protected $hidden = ['password', 'remember_token'];
 
     protected function casts(): array
     {
-        return ['email_verified_at' => 'datetime', 'password' => 'hashed'];
+        return [
+            'email_verified_at' => 'datetime',
+            'business_super_admin_approved_at' => 'datetime',
+            'business_rejected_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
