0306d77a63ca0e56558c4116264e9f862807b94b
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-17 09:52:33 +0100

    Add program business ownership relation

--- app/Models/PartnershipProgram.php
+++ app/Models/PartnershipProgram.php
@@ -6,55 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class PartnershipProgram extends Model
 {
-    //
     protected $fillable = [
-        'name',
-        'slug',
-        'description',
-        'status',
-        'starts_at',
-        'ends_at',
-        'attribution_window_days',
-        'minimum_payout',
-        'settings',
+        'owner_id', 'name', 'slug', 'description', 'status', 'starts_at', 'ends_at',
+        'attribution_window_days', 'minimum_payout', 'settings',
     ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
     public function products()
     {
-        return $this->belongsToMany(
-            Product::class,
-            'program_products',
-            'program_id',
-            'product_id'
-        );
+        return $this->belongsToMany(Product::class, 'program_products', 'program_id', 'product_id');
     }
 
-    public function partners()
-    {
-        return $this->hasMany(ProgramPartner::class, 'program_id');
-    }
-
-    public function commissionRules()
-    {
-        return $this->hasMany(CommissionRule::class, 'program_id');
-    }
-
-    public function orders()
-    {
-        return $this->hasMany(Order::class, 'program_id');
-    }
-
-    public function commissions()
-    {
-        return $this->hasMany(Commission::class, 'program_id');
-    }
-
-    public function payouts()
-    {
-        return $this->hasMany(Payout::class, 'program_id');
-    }
-
-    public function clicks()
-    {
-        return $this->hasMany(Click::class, 'program_id');
-    }
+    public function partners() { return $this->hasMany(ProgramPartner::class, 'program_id'); }
+    public function commissionRules() { return $this->hasMany(CommissionRule::class, 'program_id'); }
+    public function orders() { return $this->hasMany(Order::class, 'program_id'); }
+    public function commissions() { return $this->hasMany(Commission::class, 'program_id'); }
+    public function payouts() { return $this->hasMany(Payout::class, 'program_id'); }
+    public function clicks() { return $this->hasMany(Click::class, 'program_id'); }
 }
