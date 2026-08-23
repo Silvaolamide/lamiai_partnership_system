7666c4d7e21a9af3470104b507fd11e5a21cb91c
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-23 16:04:06 +0100

    Store program enrollment approval context

--- app/Models/ProgramPartner.php
+++ app/Models/ProgramPartner.php
@@ -6,16 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProgramPartner extends Model
 {
-    //
     protected $fillable = [
         'program_id',
         'user_id',
         'partner_code',
         'status',
+        'approval_context',
         'parent_partner_id',
         'joined_at',
         'approved_at',
     ];
+
     public function program()
     {
         return $this->belongsTo(PartnershipProgram::class);
@@ -28,57 +29,36 @@ class ProgramPartner extends Model
 
     public function parentPartner()
     {
-        return $this->belongsTo(
-            ProgramPartner::class,
-            'parent_partner_id'
-        );
+        return $this->belongsTo(ProgramPartner::class, 'parent_partner_id');
     }
 
     public function childPartners()
     {
-        return $this->hasMany(
-            ProgramPartner::class,
-            'parent_partner_id'
-        );
+        return $this->hasMany(ProgramPartner::class, 'parent_partner_id');
     }
 
     public function referrals()
     {
-        return $this->hasMany(
-            Referral::class,
-            'parent_partner_id'
-        );
+        return $this->hasMany(Referral::class, 'parent_partner_id');
     }
 
     public function orders()
     {
-        return $this->hasMany(
-            Order::class,
-            'partner_id'
-        );
+        return $this->hasMany(Order::class, 'partner_id');
     }
 
     public function commissions()
     {
-        return $this->hasMany(
-            Commission::class,
-            'partner_id'
-        );
+        return $this->hasMany(Commission::class, 'partner_id');
     }
 
     public function payouts()
     {
-        return $this->hasMany(
-            Payout::class,
-            'partner_id'
-        );
+        return $this->hasMany(Payout::class, 'partner_id');
     }
 
     public function clicks()
     {
-        return $this->hasMany(
-            Click::class,
-            'partner_id'
-        );
+        return $this->hasMany(Click::class, 'partner_id');
     }
 }
