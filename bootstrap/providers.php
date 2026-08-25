ced32a7081a574e1f8e21e9ab97d7ee218b4730e
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-25 23:10:37 +0100

    Remove duplicate Social Follow route provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Providers\AppServiceProvider;
-use App\Providers\SocialFollowServiceProvider;
 
 return [
     AppServiceProvider::class,
-    SocialFollowServiceProvider::class,
 ];
