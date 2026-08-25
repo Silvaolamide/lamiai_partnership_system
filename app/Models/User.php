4f242a04ac3ee10bc2789073bd85c4b2cb6bf917
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-25 21:49:12 +0100

    Replace Business relation with social feature ownership on User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,22 +7,18 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
     use HasRoles;
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     public function programPartners() { return $this->hasMany(ProgramPartner::class); }
-    public function business(): HasOne { return $this->hasOne(Business::class); }
+    public function socialAccounts(): HasMany { return $this->hasMany(SocialAccount::class); }
+    public function socialFollowCampaigns(): HasMany { return $this->hasMany(SocialFollowCampaign::class); }
 
     protected $fillable = ['name', 'email', 'password'];
     protected $hidden = ['password', 'remember_token'];
-
-    protected function casts(): array
-    {
-        return ['email_verified_at' => 'datetime', 'password' => 'hashed'];
-    }
+    protected function casts(): array { return ['email_verified_at' => 'datetime', 'password' => 'hashed']; }
 }
